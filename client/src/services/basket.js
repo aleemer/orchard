2b865f447ce54d023d546331b8d43f8b7eb608c3
aleemer <aleemertariq@gmail.com>
2024-02-28 18:59:13 -0800

    A few small changes, also added fruit services.

---
+++ client/src/services/basket.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+const baseURL = '/api/baskets'
+
+const getBaskets = () => {
+  const request = axios.get(baseURL)
+  return request.then(response => response.data)
+}
+
+const getBasket = (basketId) => {
+  const request = axios.get(`${baseURL}/${basketId}`)
+  return request.then(response => response.data)
+}
+
+const addBasket = (newBasket, personId) => {
+  const request = axios.post(`${baseURL}/${personId}`, newBasket)
+  return request.then(response => response.data)
+}
+
+const removeBasket = (personId, basketId) => {
+  const request = axios.delete(`${baseURL}/${basketId}`, { personId })
+  return request.then(response => response.data)
+}
+
+export default { getBaskets, getBasket, addBasket, removeBasket }
---
+++ client/src/services/fruit.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+const baseURL = '/api/fruit'
+
+const getFruit = () => {
+  const request = axios.get(baseURL)
+  return request.then(response => response.data)
+}
+
+const getOneFruit = (fruitId) => {
+  const request = axios.get(`${baseURL}/${fruitId}`)
+  return request.then(response => response.data)
+}
+
+const addFruit = (newFruit, basketId) => {
+  const request = axios.post(`${baseURL}/${basketId}`, newFruit)
+  return request.then(response => response.data)
+}
+
+const removeFruit = (basketId, fruitId) => {
+  const request = axios.delete(`${baseURL}/${fruitId}`, { basketId} )
+  return request.then(response => response.data)
+}
+
+export default { getFruit, getOneFruit, addFruit, removeFruit }
